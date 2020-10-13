2b03bb053d7a07a1131390657cd7f2f5b9778853
Nico <nicolas@awardforce.com>
2020-10-13 17:06:04 -0300

    update api key link

--- admin/views/award-force-sso-settings.php
+++ admin/views/award-force-sso-settings.php
@@ -10,14 +10,14 @@
                 <th scope="row">Award Force URL</th>
                 <td>
                     https://<input type="text" name="award-force-sso-installation-domain" value="<?php echo get_option( 'award-force-sso-installation-domain' ); ?>" size="30">
-                    <p class="description" id="tagline-description">For example: api.awardsplatform.com</p>
+                    <p class="description" id="tagline-description">For example: demo.awardsplatform.com</p>
                 </td>
             </tr>
             <tr valign="top">
                 <th scope="row">API Key</th>
                 <td>
                     <input type="text" name="award-force-sso-api-key" value="<?php echo get_option( 'award-force-sso-api-key' ); ?>" size="58" />
-                    <p class="description" id="tagline-description">An Awards Manager can generate this key in Award Force (Settings > General > Integration)</p>
+                    <p class="description" id="tagline-description">An Awards Manager can generate this key in Award Force (Settings > Developers > API keys)</p>
                 </td>
             </tr>
         </table>
